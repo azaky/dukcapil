0328bfc314a146e2417d1773a73bba90751a24c7
Luthfi Hamid Masykuri <luthfi_hamid_m@yahoo.co.id>
2015-04-06 02:21:16 +0700

    add sidebar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
     <link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/css/simple-sidebar.css') }}" rel="stylesheet">
     <link href="{{ asset('/assets/css/bootflat.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/css/style.css') }}" rel="stylesheet">
 
     <!-- Page-Specific Stylesheets -->
     @yield('stylesheet')
@@ -20,41 +23,44 @@
 </head>
 
 <body style="background-color: whitesmoke;">
-<div class="wrapper">
-    @include('app.navbar')
+@include('app.navbar')
+<div id="wrapper">
+<!-- Sidebar -->
+@include('app.left_sidebar')
+<!-- /#sidebar-wrapper -->
 
-    <div class="container-fluid documents">
+<!-- Page Content -->
+<div id="page-content-wrapper">
+    <div class="container-fluid">
         <div class="row">
-
-            <div class="col-md-2">
-                @include('app.left_sidebar')
-            </div>
-
-            <div class="col-md-8">
+            <div class="col-lg-12">
                 @yield('content')
             </div>
-
-            <div class="col-md-2">
-                @include('app.right_sidebar')
-            </div>
-
         </div>
     </div>
-
-    <div class="footer_push"></div>
 </div>
+<!-- /#page-content-wrapper -->
 
-@include('app.footer')
+</div>
+<!-- /#wrapper -->
 
 <!-- Scripts -->
 <!-- Bootstrap -->
-<script src="{{ asset('assets/js/jquery-1.11.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-1.10.1.min.js') }}"></script>
 <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
 <!-- Bootflat's JS files.-->
 <script src="{{ asset('assets/js/icheck.min.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.fs.selecter.min.js') }}"></script>
 <script src="{{ asset('assets/js/jquery.fs.stepper.min.js') }}"></script>
+
+<!-- Menu Toggle Script -->
+<script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+</script>
 
 <!-- Page-Specific Scripts -->
 @yield('script')
---
+++ resources/views/app/left_sidebar.blade.php
@@ -0,0 +1,30 @@
+<div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+            <a href="#">
+                Instansi Kependudukan
+            </a>
+        </li>
+        <li>
+            <a href="#">Dashboard</a>
+        </li>
+        <li class="dropdown">
+            <a href="#">Shortcuts</a>
+        </li>
+        <li>
+            <a href="#">Overview</a>
+        </li>
+        <li>
+            <a href="#">Events</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Services</a>
+        </li>
+        <li>
+            <a href="#">Contact</a>
+        </li>
+    </ul>
+</div>
