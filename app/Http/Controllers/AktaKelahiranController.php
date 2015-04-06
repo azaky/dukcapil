b6b59b4c61dc907ed8dac495f2c48a15383920b3
Luthfi Hamid Masykuri <luthfi_hamid_m@yahoo.co.id>
2015-04-07 00:22:10 +0700

    Try Akta Lahir

--- app/Http/Controllers/AktaKelahiranController.php
+++ app/Http/Controllers/AktaKelahiranController.php
@@ -1,11 +1,13 @@
 <?php namespace App\Http\Controllers;
 
+use App\AktaKelahiran;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use App\Penduduk;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
+use Carbon\Carbon;
 
 class AktaKelahiranController extends Controller {
 
@@ -20,6 +22,9 @@ class AktaKelahiranController extends Controller {
         return view('pages.akta_kelahiran');
 	}
 
+    /**
+     *
+     */
     public function postBuat()
     {
         $penduduk = new Penduduk();
@@ -27,6 +32,16 @@ class AktaKelahiranController extends Controller {
         $penduduk->kewarganegaraan = "wni";
         $penduduk->jenisKelamin = Input::get("jenisKelamin");
         $penduduk->tempatLahir = Input::get("tempatLahir");
-
+        $penduduk->waktuLahir = Carbon::parse(Input::get("tanggalLahir")." ".Input::get("jamLahir"));
+        if ( $penduduk->save())
+        {
+            $aktalahir = new AktaKelahiran();
+            $aktalahir->idPenduduk = $penduduk->id;
+            if ($aktalahir->save())
+            {
+                return "masuk";
+            }
+        }
+        return "gagal";
     }
 }
